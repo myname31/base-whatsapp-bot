d55912babba3cdcb597fa17b6399c35e54bcbf03
ZanskySlebew <84800435+myname31@users.noreply.github.com>
2023-04-11 21:54:44 +0700

    Add files via upload

---
+++ plugins/new.js
@@ -0,0 +1,17 @@
+var { Cooldown } = require('@kazedevid/whasapi')
+module.exports = {
+    name: "cek", //name cmd
+    aliases: ["cek", "tes", "test"], //alias
+    code: async (nky) => { //code
+    //example cooldown cmd
+        var cd = new Cooldown(nky, 5000); //5s maybe?
+        if (cd.onCooldown) return nky.reply({ text: `slow down... wait ${cd.timeleft}ms` })
+        /* I don't think it's suitable to add this
+        cd.on("end", () => {
+            nky.reply({ text: "cd timeout!, please use again..." });
+        });
+        */
+        //
+        nky.reply({ text: "On Cuy!" });
+    },
+};
