7242a997040327e4de7b26b982f15f3f34fc918b
Iqbal <95006514+IqbalzzX@users.noreply.github.com>
2023-04-12 23:26:02 +0700

    Create eval.js

--- plugins/eval.js
+++ plugins/eval.js
@@ -2,7 +2,7 @@ module.exports = {
   name: ">",
   nonPrefixed: true,
   code: async (nky) => {
-    if (nky.isOwner() == false) return
+    //if (nky.isOwner() == false) return
     if (nky.args.join(" ").length < 1) return nky.reply({text: `Masukan Text String`})
     try {
       var evaled = await eval(nky.args.join(" "));
