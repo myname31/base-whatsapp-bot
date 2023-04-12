eec4b7a126c1865a1f16cc00a9fcec59b94c8c36
ZanskySlebew <84800435+myname31@users.noreply.github.com>
2023-04-12 13:04:25 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+global.owner = ['0'] //number owner (example 628580001111)
+global.ownerSplit = ['+0'] //example +62 858-000-1111
+global.ownerName = 'Nothky' //your name
+global.prefix = '!' //prefix your bot
+global.dirsesi = 'session' //dirsesion
