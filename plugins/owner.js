ac7172c7abe5fe6f869488328d85abaf64130cf2
ZanskySlebew <84800435+myname31@users.noreply.github.com>
2023-04-12 13:04:05 +0700

    Add files via upload

---
+++ plugins/owner.js
@@ -0,0 +1,16 @@
+var { delay } = require('@adiwajshing/baileys')
+module.exports = {
+    name: 'owner',
+    aliases: ['owner', 'creator'],
+    code: async (owner) => {
+var vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+            + 'VERSION:3.0\n' 
+            + 'FN:'+global.ownerName+'\n' // full name
+            + 'ORG:By-myname31;\n' // the organization of the contact
+            + 'TEL;type=CELL;type=VOICE;waid='+global.owner+':'+global.ownerSplit+'\n' // WhatsApp ID + phone number
+            + 'END:VCARD'
+await owner.reply({ contacts: { displayName: global.ownerName, contacts: [{ vcard }] }});
+await delay(1500)
+owner.reply({ text: "Those are my owner's contacts!!" })
+},
+};
